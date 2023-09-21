0e8327183bd88e993dd85ada482f1afa01f9d87e
eduardo01322 <eduardo.errea32@gmail.com>
2023-09-21 13:29:49 -0300

    .

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BuscaCep from "../components/BuscaCepForm";
+import Cadastro from "../components/Cadastro";
+import Listagem from "../components/Listagem";
 
 const AppRouter = () => {
 
@@ -8,8 +10,8 @@ const AppRouter = () => {
         <BrowserRouter>
         <Routes>
             <Route path="buscacep" element={<BuscaCep />}/>
-            <Route path="cadastro" element={<BuscaCep />}/>
-            <Route path="listagem" element={<BuscaCep />}/>
+            <Route path="cadastro" element={<Cadastro />}/>
+            <Route path="listagem" element={<Listagem />}/>
         </Routes>
         </BrowserRouter>
     );
